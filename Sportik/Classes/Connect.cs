862df9fd55b56e576ba151833ead46b0369a1a04
Avar1tia <go-mat@mail.ru>
2023-12-15 01:00:20 +0300

    gg

---
+++ Sportik/Classes/Connect.cs
@@ -0,0 +1,14 @@
+﻿using sportik.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sportik.Classes
+{
+    class Connect
+    {
+        public static sportclubbd modeldb;
+    }
+}
